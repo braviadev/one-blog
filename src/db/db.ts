9f0e9c1cc36a6d868a33ee85623725ec01a8d4ab
Olanrewaju Toyyib <olarewajutoyyib@gmail.com>
2026-04-21 01:55:32 +0100

    Update db.ts

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,9 +1,18 @@
 import 'server-only'
 
 import { drizzle } from 'drizzle-orm/node-postgres'
-
+import { Pool } from 'pg'
 import { env } from '@/env'
 
 import * as schema from './schema'
 
-export const db = drizzle({ connection: env.DATABASE_URL, schema })
+// 1. Create a connection pool and forcefully enable SSL
+const pool = new Pool({
+  connectionString: env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+})
+
+// 2. Initialize Drizzle with the new pool
+export const db = drizzle({ client: pool, schema })
